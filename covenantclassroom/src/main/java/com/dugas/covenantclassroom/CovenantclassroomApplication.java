da0a947b6eaa63fbff34a6ab7330e2e561309d5f
Joshua-Dugas <joshua.dugas@proton.me>
2025-11-22 13:46:27 -0600

    Beginning Spring/Docket setup

---
+++ covenantclassroom/src/main/java/com/dugas/covenantclassroom/CovenantclassroomApplication.java
@@ -0,0 +1,13 @@
+package com.dugas.covenantclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovenantclassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovenantclassroomApplication.class, args);
+	}
+
+}
